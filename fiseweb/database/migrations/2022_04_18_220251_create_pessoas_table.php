6389f59cbb89093b502957185cc494a1914f23c3
BRUNO CAVALCANTE PEREIRA <93294508+Bhuuno@users.noreply.github.com>
2022-04-19 21:15:17 -0300

    Criando migrations

--- fiseweb/database/migrations/2022_04_18_220251_create_pessoas_table.php
+++ fiseweb/database/migrations/2022_04_18_220251_create_pessoas_table.php
@@ -14,8 +14,14 @@ return new class extends Migration
     public function up()
     {
         Schema::create('pessoas', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table->increments('id');
+                $table->string('rg',12);
+                $table->string('cpf',14);
+                $table->string('endereco');
+                $table->string('email');
+                $table->string('telefone','15');
+                $table->date('data_nascimento');
+                $table->string('estado_civil');
         });
     }
 
