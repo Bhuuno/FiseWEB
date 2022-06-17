8e095ab89ae7ecce8be3ed9d83d4f5d18daaa3b3
BRUNO CAVALCANTE PEREIRA <93294508+Bhuuno@users.noreply.github.com>
2022-06-17 19:38:58 -0300

    Bruno

--- fiseweb/resources/views/home.blade.php
+++ fiseweb/resources/views/home.blade.php
@@ -4,33 +4,46 @@
 
 @section('conteudo')
 
-  <div class="barra">
-      <h1 class="logo_barra">Fise<span class="logo_barra">WEB</span></h1>
+  <div class="principal container">
+    <h1 class="logo">Fise<span class="logo">WEB</span></h1>
   </div>
-  <div class="barra_menu position-relative">
-    <div class="menu position-absolute top-0 start-0">
-      <ul class="nav menu nav-tabs">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link">Disabled</a>
-        </li>
-      </ul>
-    </div>
-    <div class="button position-absolute top-0 end-0">
-      <button type="button" class="btn acesso btn-warning">Entrar</button>
-      <button type="button" class="btn acesso btn-warning">Registrar</button>
-    </div>
+  <div class="container">
+    <nav class="navbar menu navbar-expand-lg bg-light">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">Home</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Dropdown
+              </a>
+              <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <li><a class="dropdown-item" href="#">Action</a></li>
+                <li><a class="dropdown-item" href="#">Another action</a></li>
+                <li><hr class="dropdown-divider"></li>
+                <li><a class="dropdown-item" href="#">Something else here</a></li>
+              </ul>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link disabled">Disabled</a>
+            </li>
+          </ul>
+          <form class="d-flex" role="search">
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success" type="submit">Search</button>
+          </form>
+        </div>
+      </div>
+    </nav>
   </div>
-
-  <div class="portfolio">
-  </div>
-
+  
 @endsection
