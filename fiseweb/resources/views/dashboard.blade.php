1b82a414d421901ba517a6f66f21ee887c5eaaeb
MatheusBote <matheusbote0801@gmail.com>
2022-07-09 14:49:26 -0300

    Dashboard-em-producao

--- fiseweb/resources/views/dashboard.blade.php
+++ fiseweb/resources/views/dashboard.blade.php
@@ -1,19 +1,46 @@
 @extends('layouts.padrao')
 
+@section('titulo', 'Cadastro')
+
+@section('conteudo')
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
+            {{ __('Perfil') }}
         </h2>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    You're logged in!
+    <div class="container-fluid">
+        <div class="row flex-nowrap ">
+            <div class="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-dark">
+                <div class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
+                    <a href="/dashboard" class="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-white text-decoration-none">
+                        <span class="fs-5 d-none d-sm-inline">Menu</span>
+                    </a>
+                    <ul>
+                        <li class="d-flex"><img src="/icons/chat-dots.svg" class="icon-space">Chat</li>
+                        <hr>
+                        <li class="d-flex"><img src="/icons/house-door.svg" class="icon-space"> home <li>
+                        <hr>
+                        <li class="d-flex"><img src="/icons/briefcase.svg" class="icon-space">Serviços</li>
+                        <hr>
+                        <li class="d-flex"><img src="/icons/journal-bookmark.svg" class="icon-space">Agenda</li>
+                        <hr>
+                        <li class="d-flex"><img src="/icons/star-half.svg" class="icon-space">Avaliações</li>
+                        <hr>
+                        <li class="d-flex"><img src="/icons/wrench-adjustable.svg" class="icon-space">Suporte</li>
+                        <hr>
+                        <li class="d-flex"><img src="/icons/gear-fill.svg" class="icon-space">Configurações</li>
+                        <hr>
+                    </ul>
+                    </div>
+                </div>
+                <div class="col py-3">
+                    <h3>Gráfico</h3>
                 </div>
             </div>
+            
         </div>
     </div>
+
 </x-app-layout>
