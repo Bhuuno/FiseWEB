6adfe04f7730e3dd811ebe9a2671f1657f786473
MatheusBote <matheusbote0801@gmail.com>
2022-08-24 19:28:13 -0300

    Subindo novamente alterações de sábado

--- fiseweb/resources/views/layouts/navigation.blade.php
+++ fiseweb/resources/views/layouts/navigation.blade.php
@@ -4,7 +4,6 @@
         <div class="flex justify-between h-16">
             <div class="flex">
                 <!-- Logo -->
-                
                 <a href="\" style="max-width: 5.5%; heigth: auto;">
                     <img src="/img/logo.png">
                 </a>
