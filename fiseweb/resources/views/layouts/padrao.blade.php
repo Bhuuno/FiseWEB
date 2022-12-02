0e63856dbd3afb50fe31f31c5cc27fa62baeb73e
Bruno Cavalcante Pereira <bhuuno>
2022-12-02 08:55:01 -0300

    Finalizando algumas partes da notificação

--- fiseweb/resources/views/layouts/padrao.blade.php
+++ fiseweb/resources/views/layouts/padrao.blade.php
@@ -41,36 +41,48 @@
     <link href="{{asset('fonts')}}">
 
     <title>@yield('titulo')</title>
-
   </head>
+
+
+
+
+
+
   <body>
     <!-- <div class="row">
       @if(session('msg'))
           <p class="msg">{{session('msg')}}</p>
       @endif
     </div> -->
-    <div class="fab">
-      <button class="main">
-        <kbd name="qtd_notificacoes" id="qtd_notificacoes" style="border-radius:27px; font-size:18px;">0</kbd>
-      </button>
-      <ul>
-        <li style="padding: 10px;">
-          <kbd>Perguntas: <span id="perg"><span id="pergunta_var">0</span></span><p id="nomes_perguntas"></p></kbd>
-        </li>
-        <li style="padding: 10px;">
-          <kbd>Respostas: <span id="resp"><span id="resposta_var">0</span></span><p id="nomes_respostas"></p></kbd>
-        </li>
-        <!-- <li>
-          <label for="opcao3">Opção 3</label>
-          <button id="opcao3">
-            ☏
-          </button>
-        </li> -->
-      </ul>
-    </div>
+    
+    @if(!empty(auth()->user()->id) && !empty(auth()->user()->role))
+      <div class="fab">
+        <button class="main">
+          <kbd name="qtd_notificacoes" id="qtd_notificacoes" style="border-radius:27px; font-size:18px;">0</kbd>
+        </button>
+        <ul>
+          <li style="padding: 10px;">
+            <kbd  id = "limpar_perguntas">Perguntas: <span id="perg"><span id="pergunta_var">0</span></span><p id="nomes_perguntas"></p></kbd>
+          </li>
+          <li style="padding: 10px;">
+            <kbd id = "limpar_resposta">Respostas: <span id="resp"><span id="resposta_var">0</span></span><p id="nomes_respostas"></p></kbd>
+          </li>
+          <!-- <li>
+            <label for="opcao3">Opção 3</label>
+            <button id="opcao3">
+              ☏
+            </button>
+          </li> -->
+        </ul>
+      </div>
+    @endif
     @yield('conteudo')
   </body>
 </html>
+
+
+
+
 
 
 <script >
@@ -81,68 +93,74 @@
       type: 'get',
       data: {},
       success: function(result ) {  
-        var notificacao = JSON.parse(result);
 
+        var notificacao = JSON.parse(result);
         var total = parseInt(notificacao[0].perguntas) + parseInt(notificacao[0].respondido);
 
         //pega o valor das notificações
         document.getElementById('qtd_notificacoes').innerHTML = total;
-
-        $("#pergunta_var").remove();
-        document.getElementById('perg').innerHTML='<span id="pergunta">0</span>';
-        document.getElementById('perg').innerHTML = notificacao[0].perguntas;
+        if(notificacao[0].perguntas > 0)
+        {
+          $("#pergunta_var").remove();
+          document.getElementById('perg').innerHTML='<span id="pergunta">0</span>';
+          document.getElementById('perg').innerHTML = notificacao[0].perguntas;
+        }
+        else
+        {
+          $("#limpar_perguntas").remove();
+        }
+        if(notificacao[0].respondido > 0)
+        {
+          $("#resposta_var").remove();
+          document.getElementById('resp').innerHTML='<span id="resposta">0</span>';
+          document.getElementById('resp').innerHTML = notificacao[0].respondido;
+        }
+        else
+        {
+          $("#limpar_resposta").remove();
+        }
         
-        $("#resposta_var").remove();
-        document.getElementById('resp').innerHTML='<span id="resposta">0</span>';
-        document.getElementById('resp').innerHTML = notificacao[0].respondido;
+        if(notificacao[0].perguntas > 0)
+        {
+          // nomes perguntas
+          $.ajax({
+            url: '/nomes_notificacao_perguntas',
+            type: 'get',
+            data: {},
+            success: function(result ) {  
+              nomes = JSON.parse(result);
+              $perguntas = "";
+              
+              
+              nomes.forEach(item => $perguntas += '<a style:"width:50px; backgorund:color:red;" href="/dashboard/prestador/'+<?php echo !empty(auth()->user()->id)?auth()->user()->id:0 ?>+'?id='+<?php echo !empty(auth()->user()->id)?auth()->user()->id:0 ?>+'/#perguntas" class="text-decoration-none" style="color:yellow;"><div style="width:230px; display:block;"><div>'+item.nome+'</div></div></a>')
+              $("#nomes_perguntas").html($perguntas); 
 
+            },
+            error: function( request, status, error ) {
+              console.log(error);
+            }
+          });
+        }
+        if(notificacao[0].respondido > 0)
+        {
+          // nomes respostas
+          $.ajax({
+            url: '/nomes_notificacao_respostas',
+            type: 'get',
+            data: {},
+            success: function(result) {  
+              nomes = JSON.parse(result);
+              $respostas = "";
+              
+              nomes.forEach(item => $respostas += '<a href="/dashboard/prestador/'+item.user_id+'?id='+item.user_id+'/#perguntas" class="text-decoration-none" style="color:yellow;"><div style="width:230px;">'+item.nome+'</div></a>')
 
-
-        // $perguntas = notificacao[0].perguntas;
-        // $("#pergunta").html($perguntas); 
-
-        
-        // $resposta = notificacao[0].respondido;
-        // $("#resposta").html($resposta); 
-
-          
-
-
-        // nomes perguntas
-        $.ajax({
-          url: '/nomes_notificacao_perguntas',
-          type: 'get',
-          data: {},
-          success: function(result ) {  
-            nomes = JSON.parse(result);
-            $perguntas = "";
-            
-            nomes.forEach(item => $perguntas += '<a style:"width:50px; backgorund:color:red;" href="/dashboard/prestador/'+item.user_id+'?id='+item.user_id+'" class="text-decoration-none" style="color:yellow;"><div style="width:230px; display:block;"><div>'+item.nome+'</div></div></a>')
-            $("#nomes_perguntas").html($perguntas); 
-
-          },
-          error: function( request, status, error ) {
-            console.log(error);
-          }
-        });
-
-        // nomes respostas
-        $.ajax({
-          url: '/nomes_notificacao_respostas',
-          type: 'get',
-          data: {},
-          success: function(result) {  
-            nomes = JSON.parse(result);
-            $respostas = "";
-            
-            nomes.forEach(item => $respostas += '<a href="/dashboard/prestador/'+item.user_id+'?id='+item.user_id+'" class="text-decoration-none" style="color:yellow;"><div style="width:230px;">'+item.nome+'</div></a>')
-
-            $("#nomes_respostas").html($respostas);
-          },
-          error: function( request, status, error ) {
-            console.log(error);
-          }
-        });
+              $("#nomes_respostas").html($respostas);
+            },
+            error: function( request, status, error ) {
+              console.log(error);
+            }
+          });
+        }
 
       },
       error: function( request, status, error ) {
