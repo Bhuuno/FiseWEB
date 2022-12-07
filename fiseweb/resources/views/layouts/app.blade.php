2cb45fccf1eb1021021bd11bf05ab9fd394b79a2
MatheusBote <matheusbote0801@gmail.com>
2022-12-07 13:10:55 -0300

    troca de background de todos as blades

--- fiseweb/resources/views/layouts/app.blade.php
+++ fiseweb/resources/views/layouts/app.blade.php
@@ -17,7 +17,7 @@
         <script src="{{ asset('js/app.js') }}" defer></script>
     </head>
     <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
+        <div class="">
             @include('layouts.navigation')
             <!-- Page Heading -->
             <header class="bg-white shadow">
